61b20aac2befb81f0e207ba7a3c6d9f81b67e20e
flodro34 <f.derosso@gmail.com>
2024-11-15 17:42:41 +0100

    Initialisation du dépôt local

---
+++ src/main/java/fr/fdr/api_transport/ApiTransportApplication.java
@@ -0,0 +1,13 @@
+package fr.fdr.api_transport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTransportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTransportApplication.class, args);
+	}
+
+}
---
+++ src/main/java/fr/fdr/api_transport/ws/HelloController.java
@@ -0,0 +1,7 @@
+package fr.fdr.api_transport.ws;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+}
